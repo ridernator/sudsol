c72aec35c1afe81d7394b25b88e9f69ebdee0fdc
Ciaron Rider <ciaron@ridernator.com>
2020-04-04 20:11:56 +0100

    Intersection Removal now works

---
+++ src/IntersectionRemoval.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+#include "IntersectionRemoval.hpp"
+
+#include "Direction.hpp"
+#include "House.hpp"
+
+namespace Rules {
+
+    IntersectionRemoval::IntersectionRemoval(DataStructures::Grid* grid) : Rule("Intersection Removal", grid) {
+    }
+
+    bool IntersectionRemoval::solve() {
+        bool returnVal = false;
+
+        for (uint64_t candidate = 0; candidate < grid->getOrder(); ++candidate) {
+            for (auto box : grid->getBoxes()) {
+                auto elements = getElementsWithCandidate(DataStructures::House::BOX, box, candidate);
+
+                if (!elements.empty()) {
+                    if (areElementsInSameHouse(elements, DataStructures::House::ROW)) {
+                        if (removeCandidatesFromHouse(candidate, DataStructures::House::ROW, *(elements.begin()), elements)) {
+                            returnVal = true;
+                        }
+                    } else if (areElementsInSameHouse(elements, DataStructures::House::COLUMN)) {
+                        if (removeCandidatesFromHouse(candidate, DataStructures::House::COLUMN, *(elements.begin()), elements)) {
+                            returnVal = true;
+                        }
+                    }
+                }
+            }
+        }
+
+        return returnVal;
+    }
+}
